87704c56dd44cb0e87070992965417b95f8d00f6
Anastasija Semen <anastasija.semen@gmail.com>
2018-10-06 01:09:51 +0300

    back to first

--- js/index.js
+++ js/index.js
@@ -2,10 +2,6 @@ let a = parseInt(prompt("Enter the number"));
 let c = Math.floor(a / 10) * 10;
 let b = [];
 
-function splitNumbers(a, c) {
-
-    
-}
  b[0] = a - Math.floor(a / 10) * 10;
 a = Math.floor(a / 10);
 b[1] = a - Math.floor(a / 10) * 10;
@@ -18,16 +14,3 @@ alert(b);
 
 let a = parseInt(prompt("Enter the number"));
 alert(b);
-
-
-function splitNumbers(n) {
-    let c = Math.floor(a / 10) * 10;
-    let b = [];
-    let i;
-    while (i <= a) {
-        fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
-        i++;
-    };
-    return splitNumbers[b];
-
-}
