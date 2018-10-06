c8813d5f9fd81050b826d523619b22b240328188
Anastasija Semen <anastasija.semen@gmail.com>
2018-10-06 10:29:24 +0300

    made

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
+// let a = parseInt(prompt("Enter the number"));
+// let c = Math.floor(a / 10) * 10;
+// let b = [];
+
+//  b[0] = a - Math.floor(a / 10) * 10;
+// a = Math.floor(a / 10);
+// b[1] = a - Math.floor(a / 10) * 10;
+//  a = Math.floor(a / 10);
+// b[2] = a - Math.floor(a / 10) * 10;
+// a = Math.floor(a / 10);
+// b[3] = a;
+// alert(b);
+
+
+// let a = parseInt(prompt("Enter the number"));
+// alert(b);
+
+
+
 let a = parseInt(prompt("Enter the number"));
-let c = Math.floor(a / 10) * 10;
 let b = [];
 
- b[0] = a - Math.floor(a / 10) * 10;
-a = Math.floor(a / 10);
-b[1] = a - Math.floor(a / 10) * 10;
- a = Math.floor(a / 10);
-b[2] = a - Math.floor(a / 10) * 10;
-a = Math.floor(a / 10);
-b[3] = a;
-alert(b);
+for (let i = 3; i >= 0; i--) {
+    b[i] = a - Math.floor(a / 10) * 10;
+    a = Math.floor(a / 10);
+    
+}
+alert(b)
 
 
-let a = parseInt(prompt("Enter the number"));
-alert(b);
